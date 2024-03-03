10e56b32cad378902435cb1afa06893382b25f88
Zohaib Arain <zobiiiarain803@gmail.com>
2024-03-04 00:24:10 +0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import ReactFlow from "reactflow";
 import "./App.css";
 import OverviewFlow from "./ReactFloww/Flow";
-
 function App() {
-  // const initialNodes = [
-  //   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  //   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  // ];
-  // const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
   return (
     <div style={{ height: "100vh", width: "100%", background: "#F1F0EE" }}>
       <OverviewFlow />
