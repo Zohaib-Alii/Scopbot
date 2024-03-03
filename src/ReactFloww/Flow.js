41adf1918a9857bddc7f299e77fe05ea36f05cd7
Zohaib Arain <zobiiiarain803@gmail.com>
2024-03-03 23:44:24 +0500

    adding antd things or some checks like wrap in array func or error handling

--- src/ReactFloww/Flow.js
+++ src/ReactFloww/Flow.js
@@ -12,107 +12,36 @@ import "reactflow/dist/style.css";
 import {
   nodes as initialNodes,
   edges as initialEdges,
-  gptResponse,
-  hardCodeEdges,
-  hardCodeNode,
 } from "./initial-elements";
-import TextUpdaterNode from "./utils";
 import { handleGptApi } from "./gptApi";
-import { useSelector } from "react-redux";
+import TextUpdaterNode from "./TextUpdaterNode";
+import { wrapInArray } from "./utils";
 
 const onInit = (reactFlowInstance) =>
   console.log("flow loaded:", reactFlowInstance);
 // const nodeTypes = { textUpdater: () => <TextUpdaterNode /> };
 let itration = 0;
 const OverviewFlow = () => {
-  const user = useSelector((state) => state.user);
-  console.log("user", user);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  // const [inputData, setInputData] = useState("");
-  let inputData = "kk";
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
-
-  let otherNode = [
-    {
-      id: "node-2",
-      type: "textUpdater",
-      targetPosition: "top",
-      position: { x: 0, y: 200 },
-      data: { label: "node 2", value: "node 2 asd" },
-    },
-    {
-      id: "node-3",
-      type: "textUpdater",
-      targetPosition: "top",
-      position: { x: 200, y: 200 },
-      data: { label: "node 3" },
-    },
-  ];
-  // const createNodes = (newtext) => {
-  //   const res = gptResponse.map((res, ind) => {
-  //     let length = nodes.length + 1;
-  //     let indd = ind == 0 ? ind - 2 : ind + 5;
-  //     let node = {
-  //       id: `node-${ind + 1}`,
-  //       type: "textUpdater",
-  //       targetPosition: "top",
-  //       position: { x: Number(`${indd}00`), y: Number(`${length}00`) },
-  //       data: {
-  //         label: `${newtext} ${ind}`,
-  //         value: res.content,
-  //         heading: res.heading,
-  //       },
-  //     };
-  //     let edge = {
-  //       id: ind,
-  //       source: `node-${itration}`,
-  //       target: `node-${ind + 1}`,
-  //       sourceHandle: "b",
-  //     };
-  //     return { edge, node };
-  //   });
-  //   itration++;
-  //   const edgess = res.map((ed) => ed.edge);
-  //   const nodess = res.map((node) => node.node);
-  //   const newNodes = [...nodes, ...nodess];
-  //   const newEdges = [...edges, ...edgess];
-  //   console.log(
-  //     "res",
-  //     res,
-  //     "before edge",
-  //     edges,
-  //     "before nodes",
-  //     nodes,
-  //     "after nodes",
-  //     nodess,
-  //     "after edges",
-  //     edgess
-  //   );
-  //   // const newEdges = [ ...edgess];
-  //   // console.log("newEdges", newEdges);
-  //   setEdges(newEdges);
-  //   setNodes(newNodes);
-  //   // setEdges(hardCodeEdges);
-  //   // setNodes(hardCodeNode);
-  // };
 
   const createNodes = ({
     gptResponse,
     nodeId,
     clickedNodePosition = { x: 0 },
   }) => {
+    console.log("createNodes func call", "gpt res", gptResponse);
     const newNodes = [];
     const newEdges = [];
-    debugger;
-    console.log("nodeId", nodeId, itration);
     // Calculate the x-coordinate for the next node with an increment of 300 pixels
     let nextX = clickedNodePosition.x - 300; // Start with 300 pixels to the right of the clicked node
     let nodesLength = nodes.length;
-    gptResponse.forEach((res, ind) => {
+    let gptUpdatedRes = wrapInArray(gptResponse);
+    gptUpdatedRes.forEach((res, ind) => {
       const length = nodes.length + 1;
       const newNode = {
         id: `node-${nodesLength > 1 ? nodesLength + ind : ind + 1}`,
@@ -120,7 +49,6 @@ const OverviewFlow = () => {
         targetPosition: "top",
         position: { x: nextX + 300 * ind, y: Number(`${length}00`) }, // Calculate x-coordinate with an increment of 300 pixels
         data: {
-          // label: `${newtext} ${ind}`,
           value: res.content,
           heading: res.heading,
         },
@@ -132,8 +60,8 @@ const OverviewFlow = () => {
         source: nodeId,
         target: `node-${nodesLength > 1 ? nodesLength + ind : ind + 1}`,
         sourceHandle: "b",
-        // type: "smoothstep",
-        type: "step",
+        type: "smoothstep",
+        // type: "step",
       };
 
       newNodes.push(newNode);
@@ -149,14 +77,12 @@ const OverviewFlow = () => {
     setEdges(updatedEdges);
   };
 
-  const handleTextChange = (newtext, node) => {
-    debugger;
-    console.log("node", node);
-    // createNodes(newtext, node.id);
-    // setInputData(newtext);
-  };
-
   const apiHandler = async ({ inputData, nodeId }) => {
+    // update the first node content
+    if (nodeId === "node-0" && inputData) {
+      nodes[0].data.value = [inputData];
+      setNodes([...nodes]);
+    }
     const res = await handleGptApi(inputData);
     const updatedRes = JSON.parse(res).epics;
     createNodes({ gptResponse: updatedRes, nodeId });
@@ -164,7 +90,6 @@ const OverviewFlow = () => {
 
   return (
     <>
-      <button onClick={apiHandler}>test gpt</button>
       <ReactFlow
         nodes={nodes}
         edges={edges}
--- src/ReactFloww/TextHandler.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const TextHandler = () => {
-  const [userInput, setUserInput] = useState("");
-  return <div>TextHandler</div>;
-};
-
-export default TextHandler;
